2b0d4b0c0b11e43382b940f2e5e206a45bf335df
Cherry <sheepareawesome777@gmail.com>
2023-07-11 02:29:44 -0400

    how tf

--- src/main.zig
+++ src/main.zig
@@ -11,20 +11,43 @@ pub fn main() !void {
     var arena = heap.ArenaAllocator.init(heap.page_allocator);
     const alloc = arena.allocator();
 
-    var client = http.Client{ .allocator = alloc };
+    // our http client, this can make multiple requests (and is even threadsafe, although individual requests are not).
+    var client = std.http.Client{
+        .allocator = alloc,
+    };
 
-    const uri = try std.Uri.parse("https://sparklet.org/");
+    // we can `catch unreachable` here because we can guarantee that this is a valid url.
+    const uri = std.Uri.parse("https://example.com") catch unreachable;
 
-    var headers = http.Headers{ .allocator = alloc };
+    // these are the headers we'll be sending to the server
+    var headers = std.http.Headers{ .allocator = alloc };
     defer headers.deinit();
 
-    try headers.append("accept", "*/*");
+    try headers.append("accept", "*/*"); // tell the server we'll accept anything
 
+    // make the connection and set up the request
     var req = try client.request(.GET, uri, headers, .{});
     defer req.deinit();
 
-    const body = undefined;
-    _ = try req.read(body);
+    // I'm making a GET request, so do I don't need this, but I'm sure someone will.
+    // req.transfer_encoding = .chunked;
+
+    // send the request and headers to the server.
+    try req.start();
+
+    // try req.writer().writeAll("Hello, World!\n");
+    // try req.finish();
+
+    // wait for the server to send use a response
+    try req.wait();
+
+    // read the content-type header from the server, or default to text/plain
+    const content_type = req.response.headers.getFirstValue("content-type") orelse "text/plain";
+    _ = content_type;
+
+    // read the entire response body, but only allow it to allocate 8kb of memory
+    const body = req.reader().readAllAlloc(alloc, 8192) catch unreachable;
+    defer alloc.free(body);
     std.debug.print("{s}", .{body});
 
     // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
