c85253d68dab1d56965edd6816109744fa83cc24
Cherry <sheepareawesome777@gmail.com>
2023-07-11 02:35:29 -0400

    Move HTTP stuff into `rq_get()`

--- src/main.zig
+++ src/main.zig
@@ -6,49 +6,15 @@
 const std = @import("std");
 const http = std.http;
 const heap = std.heap;
+const mem = std.mem;
 
 pub fn main() !void {
     var arena = heap.ArenaAllocator.init(heap.page_allocator);
     const alloc = arena.allocator();
 
-    // our http client, this can make multiple requests (and is even threadsafe, although individual requests are not).
-    var client = std.http.Client{
-        .allocator = alloc,
-    };
-
-    // we can `catch unreachable` here because we can guarantee that this is a valid url.
-    const uri = std.Uri.parse("https://example.com") catch unreachable;
-
-    // these are the headers we'll be sending to the server
-    var headers = std.http.Headers{ .allocator = alloc };
-    defer headers.deinit();
-
-    try headers.append("accept", "*/*"); // tell the server we'll accept anything
-
-    // make the connection and set up the request
-    var req = try client.request(.GET, uri, headers, .{});
-    defer req.deinit();
-
-    // I'm making a GET request, so do I don't need this, but I'm sure someone will.
-    // req.transfer_encoding = .chunked;
-
-    // send the request and headers to the server.
-    try req.start();
-
-    // try req.writer().writeAll("Hello, World!\n");
-    // try req.finish();
-
-    // wait for the server to send use a response
-    try req.wait();
-
-    // read the content-type header from the server, or default to text/plain
-    const content_type = req.response.headers.getFirstValue("content-type") orelse "text/plain";
-    _ = content_type;
-
-    // read the entire response body, but only allow it to allocate 8kb of memory
-    const body = req.reader().readAllAlloc(alloc, 8192) catch unreachable;
-    defer alloc.free(body);
-    std.debug.print("{s}", .{body});
+    var res = try rq_get(alloc);
+    defer alloc.free(res);
+    std.debug.print("{s}", .{res});
 
     // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
     std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
@@ -65,13 +31,36 @@ pub fn main() !void {
     try bw.flush(); // don't forget to flush!
 }
 
-// pub fn rq_get() []u8 {
-//     return;
-// }
+// https://zig.news/nameless/coming-soon-to-a-zig-near-you-http-client-5b81
+pub fn rq_get(alloc: mem.Allocator) ![]u8 {
+    var client = std.http.Client{
+        .allocator = alloc,
+    };
+
+    const uri = std.Uri.parse("https://example.com") catch unreachable;
+    var headers = std.http.Headers{ .allocator = alloc };
+    defer headers.deinit();
+
+    try headers.append("accept", "*/*"); // tell the server we'll accept anything
+    var req = try client.request(.GET, uri, headers, .{});
+    defer req.deinit();
+
+    // req.transfer_encoding = .chunked;
+    try req.start();
+
+    // try req.writer().writeAll("Hello, World!\n");
+    // try req.finish();
+
+    // wait for the server to send use a response
+    try req.wait();
+    const body = req.reader().readAllAlloc(alloc, 8192) catch unreachable;
+
+    return body;
+}
 
 test "simple test" {
     var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
+    defer list.deinit();
     try list.append(42);
     try std.testing.expectEqual(@as(i32, 42), list.pop());
 }
