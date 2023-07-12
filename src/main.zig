cf59d94dc533e78d80a983a6d297d0e18825c3ee
Cherry <sheepareawesome777@gmail.com>
2023-07-12 10:43:55 -0400

    split `rq_get()` to `util.zig` + use GPA

--- src/main.zig
+++ src/main.zig
@@ -4,13 +4,22 @@
 //                     - Cherry <3 //
 /////////////////////////////////////
 const std = @import("std");
-const hzzp = @import("hzzp");
+const util = @import("util.zig");
+const rq_get = util.rq_get;
 const heap = std.heap;
 const mem = std.mem;
+const ArrayList = std.ArrayList;
 
 pub fn main() !void {
-    var arena = heap.ArenaAllocator.init(heap.page_allocator);
-    const alloc = arena.allocator();
+    var gpa = heap.GeneralPurposeAllocator(.{}){};
+    defer {
+        const st = gpa.deinit();
+        if (st == .leak) {
+            std.debug.print("leaked (bruh)", .{});
+        }
+    }
+
+    const alloc = gpa.allocator();
 
     var res = try rq_get(alloc, "https://sparklet.org/");
     defer alloc.free(res);
@@ -30,34 +39,6 @@ pub fn main() !void {
 
     try bw.flush(); // don't forget to flush!
 }
-
-// https://zig.news/nameless/coming-soon-to-a-zig-near-you-http-client-5b81
-pub fn rq_get(alloc: mem.Allocator, url: []const u8) ![]u8 {
-    var client = std.http.Client{
-        .allocator = alloc,
-    };
-
-    const uri = std.Uri.parse(url) catch unreachable;
-    var headers = std.http.Headers{ .allocator = alloc };
-    defer headers.deinit();
-
-    try headers.append("accept", "*/*"); // tell the server we'll accept anything
-    var req = try client.request(.GET, uri, headers, .{});
-    defer req.deinit();
-
-    // req.transfer_encoding = .chunked;
-    try req.start();
-
-    // try req.writer().writeAll("Hello, World!\n");
-    // try req.finish();
-
-    // wait for the server to send use a response
-    try req.wait();
-    const body = req.reader().readAllAlloc(alloc, 8192) catch unreachable;
-
-    return body;
-}
-
 test "simple test" {
     var list = std.ArrayList(i32).init(std.testing.allocator);
     defer list.deinit();
---
+++ src/util.zig
@@ -0,0 +1,31 @@
+const std = @import("std");
+const heap = std.heap;
+const mem = std.mem;
+const ArrayList = std.ArrayList;
+
+// https://zig.news/nameless/coming-soon-to-a-zig-near-you-http-client-5b81
+pub fn rq_get(alloc: mem.Allocator, url: []const u8) ![]u8 {
+    var client = std.http.Client{
+        .allocator = alloc,
+    };
+
+    const uri = std.Uri.parse(url) catch unreachable;
+    var headers = std.http.Headers{ .allocator = alloc };
+    defer headers.deinit();
+
+    try headers.append("accept", "*/*"); // tell the server we'll accept anything
+    var req = try client.request(.GET, uri, headers, .{});
+    defer req.deinit();
+
+    // req.transfer_encoding = .chunked;
+    try req.start();
+
+    // try req.writer().writeAll("Hello, World!\n");
+    // try req.finish();
+
+    // wait for the server to send use a response
+    try req.wait();
+    const body = req.reader().readAllAlloc(alloc, 8192) catch unreachable;
+
+    return body;
+}
