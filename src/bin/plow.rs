c36f59784fcdd5adcd3c4dabcf4074f2f76b5dbe
Cherry <cherry@sparklet.org>
2024-07-20 00:17:14 -0400

    parse subcommands

--- src/bin/plow.rs
+++ src/bin/plow.rs
@@ -1,3 +1,51 @@
-fn main() {
-    todo!();
+//!
+//! PLugins nOW! A simple plugin manager
+//!
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = std::env::args().collect::<Vec<String>>();
+    let Some(subcommand) = args.get(1) else {
+        show_usage(true);
+        unreachable!()
+    };
+
+    match subcommand.to_lowercase().as_str() {
+        "search" => todo!(),
+        "install" => todo!(),
+        "uninstall" => todo!(),
+        "reinstall" => todo!(),
+        "tui" => todo!(),
+        _ => {
+            eprintln!("Unknown subcommand: {}", subcommand);
+            show_usage(true);
+        }
+    }
+
+    Ok(())
+}
+
+fn show_usage(exit: bool) {
+    eprintln!(
+        r#"Usage: plow <subcommand> <args>
+Subcommands:
+    - `search <query>`
+    Search for plugins
+
+    - `install <plugin> @<version>`
+    Install a plugin
+
+    - `uninstall <plugin>`
+    Uninstall a plugin
+
+    - `reinstall <plugin> @<version>`
+    Uninstall and reinstall a plugin
+
+    - `tui`
+    Run an interactive version of the app.
+"#
+    );
+
+    if exit {
+        std::process::exit(1);
+    }
 }
